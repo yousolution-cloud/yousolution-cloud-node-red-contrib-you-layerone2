499fd453f541413af3e3b95e97fb971f6c78f031
apxc <alessio.pellizzaro@innotech.email>
2024-08-12 15:10:38 +0200

    BugFix Node pathSAP for manage response by LayerOne2

--- node/input/sapservicelayer/patchSap.js
+++ node/input/sapservicelayer/patchSap.js
@@ -24,12 +24,11 @@ module.exports = function (RED) {
         const options = { method: 'PATCH', hasRawQuery: false, hasEntityId: true, data: data };
         const login = Support.login;
         const result = await Support.sendRequest({ node, msg, config, axios, login, options });
-        msg.payload = VerifyErrorLayerOneSL(node, msg, result.data, true); // Empty Response it's positive response of ServiceLayer
+        node.log(result.data);
+        msg.payload = VerifyErrorLayerOneSL(node, msg, result.data, true);  // Empty Response it's positive response of ServiceLayer
         msg.statusCode = result.status;
-        if(msg.payload){
-          node.status({ fill: 'green', shape: 'dot', text: 'success' });
-          node.send(msg);
-        }
+        node.status({ fill: 'green', shape: 'dot', text: 'success' });
+        node.send(msg);
       } catch (error) {
         node.status({ fill: 'red', shape: 'dot', text: 'Error' });
         done(error);
